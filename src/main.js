8f91c00f5fd583c7c682e174acaa62a0debfe825
andremgrr <112595875+andremgrr@users.noreply.github.com>
2023-03-22 13:02:41 -0400

    edits

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/HTx9mmFHKH5KbTUc/scene.splinecode');
